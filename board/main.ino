8398112e9b989f25b191fdb6c4e247e752cd3e4a
Petr Nymsa <nymsa.petr@outlook.cz>
2019-04-24 23:51:51 +0200

    more work

--- board/main.ino
+++ board/main.ino
@@ -4,17 +4,24 @@
 #define SERVER_IP "192.168.0.129"
 #define SERVER_PORT 5000
 #define SERVER_IP_PORT "192.168.0.129:5000"
+#define API_DHT11 "/api/values"
+#define API_ESES "/api/flower"
 // ---------- PINS -------------------------
 #define DHTPIN 5
 #define LED_DHT11 6
 #define LED_ESES 7
 #define LED_WEB 8
 #define LED_OK 9
-// -------- TIME INTERVALS -----------
-#define INTERVAL_DHT11 3000 // each 3 seconds
-#define INTERVAL_ESES 6000  // each 6 seconds
-// -----------------------------------------
 
+#define PIN_ESES_D 3
+#define PIN_ESES_VCC 2
+#define PIN_ESES_ANALOG A0
+// -------- TIME INTERVALS -----------
+#define INTERVAL_DHT11 5000 // each 3 seconds
+#define INTERVAL_ESES 10000 // each 6 seconds
+// -----------------------------------------
+#define ESES_THRESHOLD 600
+//-----------------------------------------
 // Device ethernet configuration
 const byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 const byte ip[] = {192, 168, 0, 222};
@@ -24,6 +31,7 @@ EthernetClient client;
 DHT dht(DHTPIN, DHT11);
 
 unsigned long timer_dht11 = 0;
+unsigned long timer_eses = 0;
 byte readFailedCount = 0;
 
 bool status_sensor_dht11 = true;
@@ -41,85 +49,88 @@ void setup()
     Serial.print(F("Assigned IP: "));
     Serial.println(Ethernet.localIP());
 
-    Serial.println("DHT TEST PROGRAM ");
-    Serial.print("LIBRARY VERSION: ");
-    Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
-
     dht.begin();
 
     pinMode(LED_OK, OUTPUT);
     pinMode(LED_WEB, OUTPUT);
     pinMode(LED_ESES, OUTPUT);
     pinMode(LED_DHT11, OUTPUT);
+
+    // setup eses
+    pinMode(PIN_ESES_ANALOG, INPUT);
+    pinMode(PIN_ESES_D, INPUT);
+    pinMode(PIN_ESES_VCC, OUTPUT);
+    digitalWrite(PIN_ESES_VCC, LOW);
+
     delay(1000);
+
+    Serial.println(F("INIT COMPLETE"));
 }
 
 void loop()
 {
     set_leds();
+
+    float room_temp = 0;
+    float room_hum = 0;
+    int eses_analog = 0;
+
+    bool eses_read = false;
+    bool dht11_read = false;
+    // pokud je rozdíl mezi aktuálním časem a posledním
+    // uloženým větší než 3000 ms, proveď měření
+    if (millis() - timer_eses > INTERVAL_ESES)
+    {
+        Serial.print(F("ESES: "));
+        read_eses(&eses_analog);
+        timer_eses = millis();
+
+        if (eses_analog > ESES_THRESHOLD)
+            status_sensor_eses = false;
+        else
+            status_sensor_eses = true;
+
+        eses_read = true;
+        Serial.println(eses_analog);
+    }
+
     if (millis() - timer_dht11 > INTERVAL_DHT11)
     {
         timer_dht11 = millis();
-        Serial.println(F("-- Reading DHT11 --"));
+        Serial.print(F("DHT11: "));
 
-        float temp = 0;
-        float hum = 0;
-        status_sensor_dht11 = read_dht11(&temp, &hum);
+        status_sensor_dht11 = read_dht11(&room_temp, &room_hum);
 
         if (status_sensor_dht11)
         {
-            Serial.print(F("Teplota: "));
-            Serial.print(temp);
+            Serial.print(room_temp);
             Serial.print(F("°C, Vlhkost: "));
-            Serial.print(hum);
+            Serial.print(room_hum);
             Serial.println(F("%"));
         }
+        dht11_read = status_sensor_dht11;
     }
 
-    delay(3000);
-
-    /* Serial.println(F("Sending.."));
-    const float temp = 22.5f;
-    const float humidity = 0.7f;
-
-    String data = String(temp);
-    data += ";";
-    data += String(humidity);
-
-    Serial.println(F(" - Post request in process - "));
-
-    if (client.connect(SERVER_IP, SERVER_PORT))
+    if (dht11_read)
     {
-        Serial.print(F(" Sending Post request "));
-
-        client.println("POST /api/values HTTP/1.1");
-        client.print("Host: ");
-        client.println(SERVER_IP_PORT);
-        client.println("Connection: close");
-        client.println("Content-Type: text/plain");
-        client.print("Content-Length: ");
-        client.println(data.length());
-        client.println(""); //-------  I missed  ""
-
-        client.print(data);
+        Serial.println(F("Sending DHT11 data"));
+        send_dht11_data(room_temp, room_hum);
     }
-    else
+
+    if (eses_read)
     {
-        Serial.println(F("Can’t reach the server"));
+        Serial.println(F("Sending ESES data"));
+        send_eses_data(eses_analog);
     }
-    client.stop();
-    delay(5000);
-     */
+
+    if (dht11_read || eses_read)
+        Serial.println(F("------------------"));
 }
 
 void set_leds()
 {
-    Serial.println(status_sensor_dht11);
-    Serial.println(status_sensor_eses);
-    Serial.println(status_web);
     if (status_sensor_dht11 && status_sensor_eses && status_web)
     {
-        Serial.println(F("AL RIGHT"));
         digitalWrite(LED_OK, HIGH);
         digitalWrite(LED_DHT11, LOW);
         digitalWrite(LED_WEB, LOW);
@@ -145,19 +156,9 @@ void set_leds()
             digitalWrite(LED_WEB, LOW);
     }
 }
-
-void send_sensor(const float &temp, const float &humidity, const float &eses_hum = -1)
-{
-    String data = String(temp);
-    data += ";";
-    data += String(humidity);
-
-    if (eses_hum >= 0)
-        data += String(eses_hum);
-
-    http_post(data);
-}
-// ---------- SENSORS RELATED ----------------------------------
+// ------------------------------------------------------------
+//                     SENSORS RELATED
+// ------------------------------------------------------------
 bool read_dht11(float *temp, float *hum)
 {
     // Reading temperature or humidity takes about 250 milliseconds!
@@ -176,19 +177,45 @@ bool read_dht11(float *temp, float *hum)
     return true;
 }
 
-float read_eses()
+float read_eses(int *analog)
 {
+    digitalWrite(PIN_ESES_VCC, HIGH);
+    delay(100);
+
+    *analog = analogRead(PIN_ESES_ANALOG);
+
+    digitalWrite(PIN_ESES_VCC, LOW);
 }
-// ---------- SERVER RELATED ----------------------------------
-void http_post(const String &data)
+// ------------------------------------------------------------
+//                     SERVER RELATED
+// ------------------------------------------------------------
+void send_dht11_data(const float &temp, const float &humidity)
 {
-    Serial.println(F(" - Post request in process - "));
+    String data = String(temp);
+    data += ";";
+    data += String(humidity);
+
+    http_post(data, API_DHT11);
+}
+
+void send_eses_data(const float &value)
+{
+    String data = String(value);
+
+    http_post(data, API_ESES);
+}
+
+void http_post(const String &data, const char *endpoint)
+{
+    Serial.println(F("HTTP: Post request in process..."));
 
     if (client.connect(SERVER_IP, SERVER_PORT))
     {
-        Serial.print(F(" Sending Post request "));
+        status_web = true;
 
-        client.println("POST /api/values HTTP/1.1");
+        client.print("POST ");
+        client.print(endpoint);
+        client.println(" HTTP/1.1");
         client.print("Host: ");
         client.println(SERVER_IP_PORT);
         client.println("Connection: close");
@@ -198,10 +225,13 @@ void http_post(const String &data)
         client.println(""); //-------  I missed  ""
 
         client.print(data);
+
+        //TODO check returned status code
     }
     else
     {
-        Serial.println(F("Can’t reach the server"));
+        Serial.println(F("ERROR: Can’t reach the server"));
+        status_web = false;
     }
     client.stop();
 }
